7c76aefecd06f35d15c1b5bb7ff2c10684b4ee8e
KrzysztofKusiak1997 <krzysztofk.kusiak97@wp.pl>
2020-04-06 12:03:45 +0200

    Create trenowanie sieci.py

---
+++ trenowanie sieci.py
@@ -0,0 +1,17 @@
+import first_network
+import second_network
+import third_network
+import fourth_network
+import fifth_network
+import sixth_network
+import seventh_network
+import eighth_network
+
+first_network.first_network()
+second_network.second_network()
+third_network.third_network()
+fourth_network.fourth_network()
+fifth_network.fifth_network()
+sixth_network.sixth_network()
+seventh_network.seventh_network()
+eighth_network.eighth_network()
